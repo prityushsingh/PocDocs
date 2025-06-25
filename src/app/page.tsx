dd86a9b9851a8b0f046f6e0ea2eb07fd46275975
Prityush Parmar <89090802+prityushsingh@users.noreply.github.com>
2025-06-25 11:41:53 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const Home = () => {
   return ( 
     <div>
-      <Button>touchmycock</Button>
+      <Button>touchme</Button>
     </div>
    );
 }
